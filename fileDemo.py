7c84860f44317ab937024f100fef9a7ad46779ce
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-12-06 13:47:00 -0500

    Updated from Brython Server: 12/6/2017 1:46:59 PM

--- fileDemo.py
+++ fileDemo.py
@@ -7,6 +7,8 @@ dictionary = open('engmix.txt')
 wordCount = 0
 
 for word in dictionary:
+    if 'lol' in word:
+        print(word.strip())
     wordCount += 1
 
 print('There are',wordCount,'words in the discitonary')
