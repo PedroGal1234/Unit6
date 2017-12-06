be899d576382df9f2310c9b7e85b6f9da4344ce9
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-12-06 13:59:28 -0500

    Updated from Brython Server: 12/6/2017 1:59:27 PM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,19 @@
+#Pedro Gallino
+#12/6/17
+#howManyWords.py prints how many of each word with cirtain charecters
+
+dictionary = open('engmix.txt')
+words = []
+for i in range(22):
+    words.append(0)
+
+for word in dictionary:
+    for i in
+    if  len word == 1:
+        words[0] == words[0]+1
+    
+
+print('Thelongest word is: ',wordCount)
+
+
 
