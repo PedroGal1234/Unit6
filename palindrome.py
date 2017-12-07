1e69009fbf9b6929a91f2160b1e133c8e4b0f841
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-12-07 14:37:44 -0500

    Updated from Brython Server: 12/7/2017 2:37:43 PM

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,14 @@
+#Pedro Gallino
+#12/7/17
+#palindrome.py - checks for the palindromes
+
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    word = word.strip()
+    x = list(word)
+    x.reverse()
+    if x == list(word):
+        print(word)
+    
 
