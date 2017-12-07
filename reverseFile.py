4754261c477b157fbd5c624aa7f9bc4c2e90e576
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-12-07 14:13:38 -0500

    Updated from Brython Server: 12/7/2017 2:13:38 PM

--- reverseFile.py
+++ reverseFile.py
@@ -1,1 +1,16 @@
+#Pedro Gallino
+#12/7/17
+#reverseFile.py - reverse a list of the users choice
+
+openFile = input('Enter a file: ')
+
+file = open(openFile)
+l = []
+for line in file:
+    l.append(line.strip())
+l.reverse()
+
+for i in l:
+    print(i)
+
 
