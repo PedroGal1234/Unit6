5bcdcd511710547d35f60ecbd791bcd889b30d68
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-12-08 14:51:51 -0500

    Updated from Brython Server: 12/8/2017 2:51:50 PM

--- HW.py
+++ HW.py
@@ -1,1 +1,38 @@
+#Pedro Gallino
+#12/8/17
+#HW.py - homework for unit 6
+
+dictionary = open('engmix.txt')
+
+word = input('Enter a word to search for: ')
+yes = False
+for w in dictionary:
+    if word == w.strip():
+        yes = True
+        break
+
+if yes == True:
+    print(word,'is in the dictionary')
+else:
+    print(word,'is not in the dictionary')
+
+
+dictionary = open('engmix.txt')
+
+neatList = []
+
+for w in dictionary:
+    neatList.append(w.strip())
+
+num = int(input('Enter a number to search for: '))
+print(neatList[num-1])
+
+openFile = input('Enter a file: ')
+file = open(openFile)
+l = []
+for line in file:
+    l.append(str(line.strip())+'!')
+
+for i in l:
+    print(i)
 
