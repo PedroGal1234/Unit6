06a0cb5cf69c58a12acfab04854f10e0a1345d20
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-12-13 13:29:19 -0500

    Updated from Brython Server: 12/13/2017 1:29:17 PM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,27 @@
+#Pedro Gallino
+#12/13/17
+#quiz6.py - my unite six quiz for proggraming
 
+"""
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    cCount = 0
+    pCount = 0
+    
+    for letter in word:
+        if letter == 'c':
+            cCount += 1
+        elif letter == 'p':
+            pCount += 1
+    if cCount == 3 and pCount == 2:
+        print(word.strip())
+"""
+
+
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    if word != '':
+        if word[0] =='r':
+            print(word.strip())
